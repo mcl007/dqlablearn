5f5da1b1c9c9f73c54a6edebdba8f30498c58981
mcl007 <michael.hendry@windowslive.com>
2024-02-27 21:03:54 +0800

    add new file

---
+++ #statement.py
@@ -0,0 +1,12 @@
+#statement
+print("learning python is fun")
+print("Hello World")
+print("Hello World cup")
+#variable & literals
+number1 = 5
+number2 = 10
+words1 = "learning python language"
+#Operators
+print(number1 + number2)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
---
+++ print('Hello Michael!').py
@@ -0,0 +1,7 @@
+print('Hello Michael!')
+print('Hello World')
+print('Riset Python Language')
+
+ 
+
+
